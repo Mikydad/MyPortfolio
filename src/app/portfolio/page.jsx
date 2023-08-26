4d0c8aee31a2c8cf95a8fdb6e9787770f90851c4
Milkesa Dereje <miki@pop-os.localdomain>
2023-08-26 06:16:54 +0300

    portfolio animation and gallery added

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,16 +1,21 @@
+import Link from 'next/link'
 import styles from './page.module.css'
-import Projects from '@/components/projects/Projects'
-import Hero from 'public/apps.jpg'
 
 const portfolio = () => {
-  const projectText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ';
   return (
     <div className={styles.container}>
-      <Projects imgSrc={Hero} text={projectText}/>
-      <Projects imgSrc={Hero} text={projectText}/>
-      <Projects imgSrc={Hero} text={projectText}/>
-      <Projects imgSrc={Hero} text={projectText}/>
-
+      <h1 className={styles.selectTitle}>Choose a gallery</h1>
+      <div className={styles.items}>
+        <Link href='/portfolio/illusturations' className={styles.item}>
+          <span className={styles.title}>Illustrations</span>
+        </Link>
+        <Link href='/portfolio/illusturations' className={styles.item}>
+          <span className={styles.title}>Websites</span>
+        </Link>
+        <Link href='/portfolio/illusturations' className={styles.item}>
+          <span className={styles.title}>Applications</span>
+        </Link>
+      </div>
     </div>
   )
 }
