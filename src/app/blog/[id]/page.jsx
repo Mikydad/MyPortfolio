c631db4190f7104e69e896457197c5027577d12c
Milkesa Dereje <miki@pop-os.localdomain>
2023-08-26 07:48:24 +0300

    blog done

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
-
+import Image from 'next/image'
+import styles from './page.module.css'
 const Post = () => {
   return (
-    <div>Post</div>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>MyBLog</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe et atque, non nihil obcaecati nobis eos, possimus mollitia debitis, dicta id optio ullam pariatur accusamus amet veniam similique? Minus, vero.
+          </p>
+          <div className={styles.author}>
+            <Image
+              src='https://images.pexels.com/photos/9739345/pexels-photo-9739345.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>Mikodad</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src='https://images.pexels.com/photos/9739345/pexels-photo-9739345.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam illum totam culpa exercitationem placeat ad perspiciatis nemo officia, harum, autem esse nesciunt vero repudiandae deleniti quos corrupti voluptatum laborum itaque? <br />
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse consectetur voluptatibus magni deleniti consequuntur aperiam sint? Numquam, commodi iusto, aliquid eius sapiente asperiores harum repellendus unde quas animi fugiat impedit.       
+        </p>
+      </div>
+    </div>
   )
 }
 
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,65 @@
+import Link from 'next/link'
 import styles from './page.module.css'
+import Image from 'next/image'
 
 const Blog = () => {
   return (
-    <div className={styles.container}>Blog</div>
+    <div className={styles.mainContainer}>
+    <Link href='/blog/testId' className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image
+        src='https://images.pexels.com/photos/17485819/pexels-photo-17485819/free-photo-of-an-artist-s-illustration-of-artificial-intelligence-ai-this-image-represents-the-ways-in-which-ai-can-solve-important-problems-it-was-created-by-vincent-schwenk-as-part-of-the-visualis.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        alt=''
+        width={400}
+        height={250}
+        className={styles.image}
+        />
+      </div>
+      <div className={styles.content}>
+        <h1 className={styles.title}>How to be good at Cooding</h1>
+        <p className={styles.desc}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br />
+        In, adipisci. Harum non neque minima quo veniam perferendis<br />
+        maiores, magni nemo accusamus tempore rerum, earum saepe <br />cupiditate
+         voluptas vitae ipsa a!</p>
+      </div>
+    </Link>
+    <Link href='/blog/testId' className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image
+        src='https://images.pexels.com/photos/17485819/pexels-photo-17485819/free-photo-of-an-artist-s-illustration-of-artificial-intelligence-ai-this-image-represents-the-ways-in-which-ai-can-solve-important-problems-it-was-created-by-vincent-schwenk-as-part-of-the-visualis.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        alt=''
+        width={400}
+        height={250}
+        className={styles.image}
+        />
+      </div>
+      <div className={styles.content}>
+        <h1 className={styles.title}>How to be good at Cooding</h1>
+        <p className={styles.desc}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br />
+        In, adipisci. Harum non neque minima quo veniam perferendis<br />
+        maiores, magni nemo accusamus tempore rerum, earum saepe <br />cupiditate
+         voluptas vitae ipsa a!</p>
+      </div>
+    </Link>
+    <Link href='/blog/testId' className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image
+        src='https://images.pexels.com/photos/17485819/pexels-photo-17485819/free-photo-of-an-artist-s-illustration-of-artificial-intelligence-ai-this-image-represents-the-ways-in-which-ai-can-solve-important-problems-it-was-created-by-vincent-schwenk-as-part-of-the-visualis.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        alt=''
+        width={400}
+        height={250}
+        className={styles.image}
+        />
+      </div>
+      <div className={styles.content}>
+        <h1 className={styles.title}>How to be good at Cooding</h1>
+        <p className={styles.desc}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br />
+        In, adipisci. Harum non neque minima quo veniam perferendis<br />
+        maiores, magni nemo accusamus tempore rerum, earum saepe <br />cupiditate
+         voluptas vitae ipsa a!</p>
+      </div>
+    </Link>
+    </div>
   )
 }
 
